4d6fe0d1069cfdc78e571d992819f861bf8ba3c9
Jose Gomez <jose@josecgomez.com>
2023-03-30 10:20:07 -0400

    Add project files.

---
+++ RestTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EpicorRestAPI;
+namespace RestTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EpicorRest.AppPoolHost = "Your.TLD.Com";
+            EpicorRest.AppPoolInstance = "YourEpicorInstance";
+            EpicorRest.APIKey = "YourAPIKey";
+            EpicorRest.UserName = "YourUserName";
+            EpicorRest.Password = "YourPassword";
+            EpicorRest.Company = "YourCompanyID";
+            EpicorRest.APIVersion = EpicorRestVersion.V2;
+            var dic = new Dictionary<string, string>();
+            dic.Add("$filter", "PartNum eq '10003'");
+            var x = EpicorRest.BoGet("Erp.BO.PartRevSearchSvc", "PartRevSearches",dic);
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(x.ResponseData));
+            Console.ReadLine();
+
+        }
+    }
+}
